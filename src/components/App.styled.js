920a9bf7f5eeafc71600d929c57e35b8925d729d
Vladislav Zmaga <vladislavzmaga@gmail.com>
2023-01-12 13:49:30 +0200

    add

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -3,9 +3,10 @@ import img from '../img/contacts.jpg';
 // const imgUrl = require('../img/contacts.jpg').default;
 
 export const AppWrapper = styled.div`
-  // background-image: url(${img})
-  //  background-size: contain;
-  background-color: bisque;
+  // background: url(${img});
+  // background-size: contain;
+  // background-color: bisque;
+  padding-bottom: 50px;
 
-  height: 100vh;
+  height: auto;
 `;
